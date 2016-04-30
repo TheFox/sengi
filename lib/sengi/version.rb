5fd8710c32f9141d6263946b76e8a3c10aee5e97
Christian Mayer <christian@fox21.at>
2016-04-30 14:35:08 +0200

    Shell variables.

--- lib/sengi/version.rb
+++ lib/sengi/version.rb
@@ -1,8 +1,8 @@
 
 module TheFox
 	module Sengi
-		VERSION = '0.1.0-dev.5'
-		DATE = '2016-04-24'
+		VERSION = '0.1.0-dev.6'
+		DATE = '2016-04-30'
 		HOMEPAGE = 'https://github.com/TheFox/sengi'
 		
 		#HTTP_USER_AGENT = "Sengi SearchENGIne/#{VERSION}"
